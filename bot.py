dead7e142ceb034420d4391b7da7bd7dcb83de89
lonelywolf1981 <andrey.lw@gmail.com>
2021-10-06 10:10:49 +0600

    Релиз 0.6 переформатирование, добавление ответов на любые другие посты кроме команд

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from handlers import client, admin, other
 
 client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
-#other.register_handlers_other(dp)
+other.register_handlers_other(dp)
 
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
--- handlers/other.py
+++ handlers/other.py
@@ -1,10 +1,12 @@
 from aiogram import types, Dispatcher
+from keyboards import kb_client
 from create_bot import bot
 
 
 async def echo_send(message: types.Message):
     await bot.send_message(message.from_user.id, 'Бот для сброса пароля\n'
-                                                 'для начала наберите команду "сброс"')
+                                                 'для начала наберите команду "сброс"',
+                           reply_markup=kb_client)
 
 
 def register_handlers_other(dp: Dispatcher):
